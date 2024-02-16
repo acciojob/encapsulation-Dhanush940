5ec8496811383a7b15cf25213f2314dff20be792
Dhanush940 <dhanushkunamaneni123@gmail.com>
2024-02-16 08:25:37 +0530

    Added getters and setters

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj=new RWOnly();
+//        obj.name="Hi";
+        obj.setName("Dhanush");
+        System.out.print(obj.getName());
+    }
 }
